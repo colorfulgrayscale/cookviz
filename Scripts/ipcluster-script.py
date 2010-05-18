c90b772fcc6975650ecb4599e472fc5fb1d7bede
Christopher Gateley <christopher.gateley@gmail.com>
2010-05-17 21:49:12 -0500

    Inital commit of working Cairo test script.

---
+++ Scripts/ipcluster-script.py
@@ -0,0 +1,9 @@
+#!C:\Users\Chris\Desktop\cookviz\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'ipython==0.10','console_scripts','ipcluster'
+__requires__ = 'ipython==0.10'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('ipython==0.10', 'console_scripts', 'ipcluster')()
+)
---
+++ Scripts/pip-script.py
@@ -0,0 +1,9 @@
+#!C:\Users\Chris\Desktop\cookviz\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==0.7.1','console_scripts','pip'
+__requires__ = 'pip==0.7.1'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('pip==0.7.1', 'console_scripts', 'pip')()
+)
